84f37d4f5bcf9b47a3ef322a8ad991c616f3fe64
Andrew <andrewandari96@gmail.com>
2025-10-27 16:19:34 -0500

    Medium - LeetCode 2: Add Two Numbers

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        int carry = 0;
+        
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+            
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+        
+        return dummy->next;
+    }
+};
+
+int main() {
+    Solution solution;
+    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+    
+    while (result != nullptr) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    cout << endl;
+    
+    return 0;
+}
